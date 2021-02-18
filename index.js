0ddb8db6ac32439ba1b12e051ff40ef4633fcdfb
Amber Chunn <a.77@me.com>
2021-02-18 01:10:21 -0500

    Added Home Router

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
-// require your server and launch it
-const server = require("./api/server")
+// Requires
+const express = require('express');
+const helmet = require("helmet")
+const cors = require("cors")
 
+// Routers
+const homeRouter = require("./api/home-router");
+const postsRouter = require('./api/posts/posts-router');
+const usersRouter = require('./api/users/users-router');
+
+// Middleware
+const { logger } = require('./api/middleware/middleware');
+
+// Server & ENV
+const server = express();
 const port = process.env.PORT || 2000;
 
+// Use Json
+server.use(express.json());
+server.use(helmet())
+server.use(cors())
+
+// Use Routes
+server.use("/", homeRouter)
+server.use("/posts", postsRouter);
+server.use("/users", usersRouter);
+server.use(logger);
+
+// Error Middleware
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    message: "Something went wrong. Please try again later.",
+  })
+  next();
+})
+
+// Server Listens
 server.listen(port, () => {
 	console.log(`Server running at http://localhost:${port}`)
 })
